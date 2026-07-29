e1080b2a29245fd56f9b8548c7c78ff387ba0f48
Ansh Gupta <anshgupta721@gmail.com>
2026-07-28 23:24:55 -0600

    added shell code for kalman filter implementation

--- crates/estimation/src/kalman_filters/kf.rs
+++ crates/estimation/src/kalman_filters/kf.rs
@@ -1,14 +1,26 @@
+use nalgebra::SVector;
 
-trait kf {
-    fn predict(); // Step dynamics forward
-    fn update(/* Measurement */); // Update based on measurements
-    fn get_state() -> <f64> ; //getter function for receiving kf state
-}
-
+use super::Kf;
+use crate::kalman_filters::kf_error::KfError;
 // TODO: KalmanDiagnostics for ensuring the filter is behaving correctly
 
-struct KalmanFilter{
+pub struct KalmanFilter<const N: usize, const M: usize> {
     // state array
     // measurements
     // other stuff
+
+}
+
+impl<const N: usize, const M: usize> Kf<N,M> for KalmanFilter<N,M>{
+    fn predict(&mut self, dt: f64){
+        
+    }
+
+    fn update(&mut self, z: &SVector<f64, M>){
+
+    }
+
+    fn get_state(&self) -> Result<SVector<f64, N>, KfError>{
+        return Err(KfError::SingularInnovationMatrix);
+    }
 }
